8313d4f56728e581aa41d6ea9fa369798d726db9
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-11-25 10:56:00 -0800

    Run Frozen-Flask from blog.py (main entry point)

--- blog.py
+++ blog.py
@@ -3,3 +3,8 @@ from app import app
 @app.shell_context_processor
 def make_shell_context():
     return {}
+
+if __name__ == '__main__':
+    from flask_frozen import Freezer
+    freezer = Freezer(app)
+    freezer.run()
